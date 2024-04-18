cb03fa3e6438c73491c90ba18d7047d7ff174c28
TimiPashov <pashov91@gmail.com>
2024-04-18 18:59:20 +0300

    install cors

--- config/express.js
+++ config/express.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 module.exports = (app) => {
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
+    app.use(cors({
+        origin: '*',
+        credentials: true
+    }));
 }
