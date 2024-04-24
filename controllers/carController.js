31e37e00c346e93d0a7afce567775ce5980ff5be
TimiPashov <pashov91@gmail.com>
2024-04-24 19:14:52 +0300

    implem delete car func

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,7 +1,11 @@
-const Car = require("../models/Car");
 const User = require("../models/User");
 
-const { getAllCars, createCar, getOneCar } = require("../services/carService");
+const {
+  getAllCars,
+  createCar,
+  getOneCar,
+  deleteCar,
+} = require("../services/carService");
 const { auth } = require("../utils/auth");
 const carController = require("express").Router();
 
@@ -36,11 +40,26 @@ carController.post("/", auth(), async (req, res) => {
   }
 });
 
-carController.get("/:carId", async (req, res) => {
+carController.get("/:carId", auth(), async (req, res) => {
   const car = await getOneCar(req.params.carId);
 
   if (car) {
-    res.status(200).json({ car });
+    res.status(200).json(car);
+  }
+});
+
+carController.delete("/:carId", auth(), async (req, res) => {
+  try {
+    const carId = req.params.carId;
+    const user = await User.findById(req.user._id);
+    const isOwner = user.cars.includes(carId);
+    if (!isOwner){
+        throw new Error('Must be owner!')
+    }
+    const car = await deleteCar(carId);
+    res.status(200).json({message: 'Car Deleted!'})
+  } catch (error) {
+    res.status(401).json({ error: error.message });
   }
 });
 
